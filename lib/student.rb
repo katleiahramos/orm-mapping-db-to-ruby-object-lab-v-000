fe80f4a2333ad49ff5d85e414e22d2a449c40342
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-10 18:54:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,6 @@ class Student
     students = DB[:conn].execute(sql)
     students_below_12 = []
     students.each do |student|
-      binding.pry
       students_below_12 << find_by_name(student[1])
     end
     students_below_12
